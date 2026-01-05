109549532ea3aebab3bc625ef7ce2d4edfb1ad07
Adriann <adrianpaslaru6969@gmail.com>
2026-01-05 11:34:45 +0200

    Main release

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,61 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+#include <Geode/fmod/fmod.hpp>
 
 using namespace geode::prelude;
 
-class $modify(PlayLayer) {
+class $modify(AmberPlayLayer, PlayLayer) {
     void destroyPlayer(PlayerObject* p0, GameObject* p1) {
         PlayLayer::destroyPlayer(p0, p1);
 
-        // show kabum
-        int currentPercent = static_cast<int>(this->getCurrentPercent());
-        
-        if (currentPercent == 16) {
-            // the audio trigger
-            FMODAudioEngine::sharedEngine()->playEffect("kabum.mp3");
-
-            // the visual overlay
-            auto cat = CCSprite::create("cat.png");
-            auto size = CCDirector::get()->getWinSize();
-            cat->setPosition({size.width / 2, size.height / 2});
+        if (static_cast<int>(this->getCurrentPercent()) == 16) {
+            auto resDir = Mod::get()->getResourcesDir();
+            auto audioPath = (resDir / "kabum.mp3").string();
             
-            this->addChild(cat, 9999); 
+            FMOD::System* system = FMODAudioEngine::sharedEngine()->m_system;
+            FMOD::Sound* sound = nullptr;
+            FMOD::Channel* channel = nullptr;
+            
+            system->createSound(
+                audioPath.c_str(),
+                FMOD_DEFAULT,
+                nullptr,
+                &sound
+            );
+            
+            if (sound) {
+                system->playSound(sound, nullptr, false, &channel);
+                
+                if (channel) {
+                    channel->setVolume(0.5f);
+                    
+                    unsigned long long dspClock;
+                    int rate;
+                    system->getSoftwareFormat(&rate, nullptr, nullptr);
+                    channel->getDSPClock(nullptr, &dspClock);
+                    
+                    channel->addFadePoint(dspClock + (rate * 1.5), 0.5f);
+                    channel->addFadePoint(dspClock + (rate * 2.5), 0.0f);
+                    channel->setDelay(0, dspClock + (rate * 2.5), true);
+                }
+            }
+
+            auto cat = CCSprite::create((resDir / "cat.png").string().c_str());
+            if (cat) {
+                auto size = CCDirector::get()->getWinSize();
+                cat->setPosition({size.width / 2, size.height / 2});
+                cat->setOpacity(0); 
+                cat->setZOrder(9999); 
+                this->addChild(cat);
+
+                cat->runAction(CCSequence::create(
+                    CCFadeIn::create(0.1f),
+                    CCDelayTime::create(1.7f),
+                    CCFadeOut::create(0.1f),
+                    CCCallFunc::create(cat, callfunc_selector(CCNode::removeFromParent)),
+                    nullptr
+                ));
+            }
         }
     }
 };
