829941d147ddb8683e40ff876c909a7b8052930f
Adriann <adrianpaslaru6969@gmail.com>
2026-01-05 10:54:39 +0200

    First release

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,19 @@ class $modify(PlayLayer) {
     void destroyPlayer(PlayerObject* p0, GameObject* p1) {
         PlayLayer::destroyPlayer(p0, p1);
 
-        if (this->getCurrentPercent() == 16) {
-            // kabum trigger
+        // show kabum
+        int currentPercent = static_cast<int>(this->getCurrentPercent());
+        
+        if (currentPercent == 16) {
+            // the audio trigger
             FMODAudioEngine::sharedEngine()->playEffect("kabum.mp3");
 
-            // the cat pfp overlay
+            // the visual overlay
             auto cat = CCSprite::create("cat.png");
             auto size = CCDirector::get()->getWinSize();
             cat->setPosition({size.width / 2, size.height / 2});
-            this->addChild(cat, 100); 
+            
+            this->addChild(cat, 9999); 
         }
     }
 };
